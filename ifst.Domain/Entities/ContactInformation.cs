3f5512342217f9648794c7d33a1e54223f559aca
Smp <alirezaqxr@gmail.com>
2024-11-19 11:00:05 +0330

    ContactInformation Model

---
+++ ifst.Domain/Entities/ContactInformation.cs
@@ -0,0 +1,12 @@
+﻿namespace ifst.API.ifst.Domain.Entities;
+
+public class ContactInformation
+{
+    public int Id { get; set; }
+    public string? Phone { get; set; }
+    public string? Number { get; set; }
+    public string? Email { get; set; }
+    public string? Address { get; set; }
+    public string? PostCode { get; set; }
+    public string? Location { get; set; }
+}
