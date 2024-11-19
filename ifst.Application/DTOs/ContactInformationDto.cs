db749e1faf4f2ff8eb6c5083d6f0df8540842186
Smp <alirezaqxr@gmail.com>
2024-11-19 12:35:39 +0330

    ContactInformationDto

---
+++ ifst.Application/DTOs/ContactInformationDto.cs
@@ -0,0 +1,11 @@
+﻿namespace ifst.API.ifst.Application.DTOs;
+
+public class ContactInformationDto
+{
+    public string? Phone { get; set; }
+    public string? Number { get; set; }
+    public string? Email { get; set; }
+    public string? Address { get; set; }
+    public int? PostCode { get; set; }
+    public string? Location { get; set; }
+}
