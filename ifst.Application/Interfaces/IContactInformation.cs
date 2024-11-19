d0df4e51db9b11d09f4278b5cdefc4ac88870d36
Smp <alirezaqxr@gmail.com>
2024-11-19 11:27:59 +0330

    Contact Information Repository and Interface

---
+++ ifst.Application/Interfaces/IContactInformation.cs
@@ -0,0 +1,8 @@
+﻿using ifst.API.ifst.Domain.Entities;
+
+namespace ifst.API.ifst.Application.Interfaces;
+
+public interface IContactInformation:IRepository<ContactInformation>
+{
+    
+}
---
+++ ifst.Infrastructure/Data/Repository/ContactInformationRepository.cs
@@ -0,0 +1,14 @@
+﻿using ifst.API.ifst.Application.Interfaces;
+using ifst.API.ifst.Domain.Entities;
+
+namespace ifst.API.ifst.Infrastructure.Data.Repository;
+
+public class ContactInformationRepository:Repository<ContactInformation>, IContactInformation
+{
+    private readonly ApplicationDbContext _context;
+
+    public ContactInformationRepository(ApplicationDbContext context) : base(context)
+    {
+        _context = context;
+    }
+}
