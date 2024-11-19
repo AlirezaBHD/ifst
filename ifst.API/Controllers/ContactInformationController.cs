6d217a150cae63bf6d93b5dce4a722a92f7d4efd
Smp <alirezaqxr@gmail.com>
2024-11-19 12:37:31 +0330

    Replace Post with Put for ContactInformation

--- ifst.API/Controllers/ContactInformationController.cs
+++ ifst.API/Controllers/ContactInformationController.cs
@@ -1,11 +1,11 @@
-﻿using ifst.API.ifst.Application.Interfaces;
+﻿using ifst.API.ifst.Application.DTOs;
+using ifst.API.ifst.Application.Interfaces;
 using ifst.API.ifst.Application.Services;
 using ifst.API.ifst.Domain.Entities;
 using ifst.API.ifst.Infrastructure.Data.Repository;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ifst.API.ifst.API.Controllers;
-
 
 [Route("api/[controller]")]
 [ApiController]
@@ -14,40 +14,44 @@ public class ContactInformationController : ControllerBase
     private readonly IContactInformationRepository _repository;
     private readonly GeneralServices _generalServices;
 
-    public ContactInformationController(IContactInformationRepository contactInformationRepository , GeneralServices generalServices)
+    public ContactInformationController(IContactInformationRepository contactInformationRepository,
+        GeneralServices generalServices)
     {
         _repository = contactInformationRepository;
         _generalServices = generalServices;
-
     }
     
-    [HttpPost("SubmitContactInformation")]
-    public async Task<IActionResult> SubmitContactInformation([FromForm] string phone, [FromForm] string number,
-        [FromForm] string email, [FromForm] string address, [FromForm] int postCode, [FromForm] string location)
-    {
-        var contactInformationObj = new ContactInformation
-        {
-            Phone = phone,
-            Number = number,
-            Email = email,
-            Address = address,
-            PostCode = postCode,
-            Location = location
-        };
-        
-        await _repository.AddAsync(contactInformationObj);
-        await _generalServices.SaveAsync();
-        
-        // var contactDtoObj = new ContactUsDto
-        // {
-        //     Id = contactObj.Id,
-        //     FullName = contactObj.FullName,
-        //     Email = contactObj.Email,
-        //     Body = contactObj.Body,
-        //     Date = contactObj.Date,
-        // };
 
-        // return Ok("success");
-        return Ok(contactInformationObj);
+    [HttpPut("UpdateContactInformation")]
+    public async Task<IActionResult> UpdateContactInformation([FromBody] ContactInformationDto dto)
+    {
+        var contactInfo = await _repository.GetByIdFirstOrDefaultAsync();
+
+        if (contactInfo == null)
+        {
+            contactInfo = new ContactInformation
+            {
+                Phone = dto.Phone,
+                Number = dto.Number,
+                Email = dto.Email,
+                Address = dto.Address,
+                PostCode = dto.PostCode,
+                Location = dto.Location,
+            };
+
+            await _repository.AddAsync(contactInfo);
+        }
+        else
+        {
+            contactInfo.Phone = dto.Phone;
+            contactInfo.Number = dto.Number;
+            contactInfo.Email = dto.Email;
+            contactInfo.Address = dto.Address;
+            contactInfo.PostCode = dto.PostCode;
+            contactInfo.Location = dto.Location;
+        }
+
+        await _generalServices.SaveAsync();
+        return Ok(dto);
     }
 }
