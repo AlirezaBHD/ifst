8d29f6a590327393093e2467a56943c674b5624f
Smp <alirezaqxr@gmail.com>
2024-11-19 10:13:11 +0330

    Image Interface and Repository

---
+++ ifst.Infrastructure/Data/Repository/ImageRepository.cs
@@ -0,0 +1,14 @@
+﻿using ifst.API.ifst.Application.Interfaces;
+using ifst.API.ifst.Domain.Entities;
+
+namespace ifst.API.ifst.Infrastructure.Data.Repository;
+
+public class ImageRepository : Repository<Image>,IImageRepository
+{
+    private readonly ApplicationDbContext _context;
+    
+    public ImageRepository(ApplicationDbContext context) : base(context)
+    {
+        _context = context;
+    }
+}
