da571e6286c67373c16948024a9fd54a1ad7b11c
Smp <alirezaqxr@gmail.com>
2024-11-19 13:44:42 +0330

    Newsletter Model

---
+++ ifst.Domain/Entities/Newsletter.cs
@@ -0,0 +1,8 @@
+﻿namespace ifst.API.ifst.Domain.Entities;
+
+public class Newsletter
+{
+    public int Id { get; set; }
+    public string ImagePath { get; set; }
+    public string FilePath { get; set; }
+}
