a7f8dd4ebc221d51483c0816fa2c279d19758845
Smp <alirezaqxr@gmail.com>
2024-11-24 14:01:20 +0330

    Add ExceptionHandler.cs

---
+++ ifst.Application/Exceptions/ExceptionHandler.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+
+namespace ifst.API.ifst.Application.Exceptions;
+
+public class ExceptionHandler : IExceptionHandler
+{
+    public ValueTask<bool> TryHandleAsync(HttpContext httpContext, Exception exception, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
