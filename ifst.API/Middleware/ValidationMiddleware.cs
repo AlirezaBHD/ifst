9c3bd6b8486cef62f1e176f1b398325ed3e9a445
Smp <alirezaqxr@gmail.com>
2024-11-24 13:59:05 +0330

    Add Validation

---
+++ ifst.API/Middleware/ValidationMiddleware.cs
@@ -0,0 +1,44 @@
+﻿using FluentValidation;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ifst.API.ifst.API.Middleware;
+
+public class ValidationMiddleware
+{
+    private readonly RequestDelegate _next;
+
+    public ValidationMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        try
+        {
+            await _next(context);
+        }
+        catch (ValidationException ex)
+        {
+            context.Response.StatusCode = StatusCodes.Status400BadRequest;
+            context.Response.ContentType = "application/json";
+
+            var problemDetails = new ValidationProblemDetails
+            {
+                Title = ".خطایی رخ داده است",
+                Status = StatusCodes.Status400BadRequest,
+                // Instance = context.Request.Path
+            };
+
+            foreach (var error in ex.Errors)
+            {
+                problemDetails.Errors.Add(error.PropertyName, new[] { error.ErrorMessage });
+            }
+
+            await context.Response.WriteAsJsonAsync(problemDetails);
+        }
+    }
+}
+
+//                Type = "https://tools.ietf.org/html/rfc9110#section-15.5.1",
+
---
+++ ifst.Application/Exceptions/ValidationExtensions.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using FluentValidation.Results;
+
+namespace ifst.API.ifst.Application.Exceptions;
+
+public static class ValidationExtensions
+{
+    public static void ThrowIfNull<T>(this T entity, string displayName) where T : class
+    {
+        if (entity == null)
+        {
+            var errors = new List<ValidationFailure>
+            {
+                new ValidationFailure(typeof(T).Name, $".{displayName} یافت نشد")
+            };
+
+            throw new ValidationException("Validation Error", errors);
+        }
+    }
+}
