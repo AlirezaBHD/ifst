d4e670099d335dba436520ece921266040c1a297
Smp <alirezaqxr@gmail.com>
2024-11-24 14:06:03 +0330

    Separate Image From Album

--- ifst.Domain/Entities/Album.cs
+++ ifst.Domain/Entities/Album.cs
@@ -2,16 +2,6 @@
 
 namespace ifst.API.ifst.Domain.Entities;
 
-[DisplayName("تصویر")]
-public class Image
-{
-    public int Id { get; set; }
-    public string Path  { get; set; }
-    public string Description  { get; set; }
-    
-    public int? AlbumId  { get; set; }
-    public Album? Album { get; set; }
-}
 
 [DisplayName("آلبوم")]
 public class Album
