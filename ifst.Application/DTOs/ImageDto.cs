fe6c7129efddbc9ce50ade1df30e5b098a732312
Smp <alirezaqxr@gmail.com>
2024-11-24 14:00:27 +0330

    Add ImageDto and Profile

---
+++ ifst.Application/DTOs/ImageDto.cs
@@ -0,0 +1,23 @@
+﻿namespace ifst.API.ifst.Application.DTOs;
+
+
+
+public class AddImageDto
+{
+    public IFormFile File { get; set; }
+    public string Description { get; set; }
+}
+
+public class GetImageDto
+{
+    public int Id { get; set; }
+}
+public class ImageDto
+{
+    public int Id { get; set; }
+    public string Path { get; set; }
+    public string Description { get; set; }
+    public int AlbumId { get; set; }
+    
+}
+
---
+++ ifst.Application/Profiles/ImageProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using ifst.API.ifst.Application.DTOs;
+using ifst.API.ifst.Domain.Entities;
+
+namespace ifst.API.ifst.Application.Profiles;
+
+public class ImageProfile: Profile
+{
+    public ImageProfile()
+    {
+        CreateMap<AddImageDto,Image >();
+        // CreateMap<GetImageDto,Image >();
+        CreateMap<Image, ImageDto>();
+    }
+}
