a09e2db9c808cca8d85056871c262361ecb84415
Smp <alirezaqxr@gmail.com>
2024-11-24 14:00:11 +0330

    Add Album Profile

---
+++ ifst.Application/Profiles/AlbumProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using ifst.API.ifst.Application.DTOs;
+using ifst.API.ifst.Domain.Entities;
+
+namespace ifst.API.ifst.Application.Profiles;
+
+public class AlbumProfile : Profile
+{
+    public AlbumProfile()
+    {
+        CreateMap<CreateAlbumDto,Album >();
+        CreateMap<GetAlbumDto,Album >();
+        CreateMap<Album, AlbumDto>();
+    }
+}
