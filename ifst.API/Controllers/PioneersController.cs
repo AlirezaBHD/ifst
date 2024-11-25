bc4bffe8474e2b839a90b2f09658dbc1173b0afb
Smp <alirezaqxr@gmail.com>
2024-11-25 09:30:15 +0330

    Completing PioneersController.cs

--- ifst.API/Controllers/PioneersController.cs
+++ ifst.API/Controllers/PioneersController.cs
@@ -2,6 +2,7 @@
 using AutoMapper;
 using ifst.API.ifst.Application.DTOs;
 using ifst.API.ifst.Application.Interfaces;
+using ifst.API.ifst.Application.Interfaces.ServiceInterfaces;
 using ifst.API.ifst.Application.Services;
 using ifst.API.ifst.Domain.Entities;
 using ifst.API.ifst.Infrastructure.FileManagement;
@@ -14,34 +15,17 @@ namespace ifst.API.ifst.API.Controllers
     [ApiController]
     public class PioneersController : ControllerBase
     {
-        private readonly IMapper _mapper;
+        private readonly IPioneersService _pioneersService;
 
-        private readonly FileService _fileService;
-        private readonly IPioneersRepository _repository;
-        private readonly GeneralServices _generalServices;
-
-        public PioneersController(IMapper mapper, FileService fileService, GeneralServices generalServices,
-            IPioneersRepository repository)
+        public PioneersController(IPioneersService pioneersService)
         {
-            _mapper = mapper;
-
-            _fileService = fileService;
-            _repository = repository;
-            _generalServices = generalServices;
+            _pioneersService = pioneersService;
         }
 
         [HttpPost("AddPioneer")]
         public async Task<IActionResult> AddPioneer([FromForm] AddPioneersDto pioneerDto)
         {
-            var path = await _fileService.SaveFileAsync(pioneerDto.File, "Pioneer");
-
-            var pioneer = _mapper.Map<Pioneers>(pioneerDto);
-            pioneer.ImagePath = path;
-
-            await _repository.AddAsync(pioneer);
-            await _generalServices.SaveAsync();
-
-            var pioneerDtoObj = _mapper.Map<PioneersDto>(pioneer);
+            var pioneerDtoObj = await _pioneersService.AddPioneerAsync(pioneerDto);
 
             return Ok(pioneerDtoObj);
         }
@@ -49,8 +33,7 @@ namespace ifst.API.ifst.API.Controllers
         [HttpGet("GetPioneer")]
         public async Task<IActionResult> GetPioneer([FromQuery] GetPioneersDto getPioneerDto)
         {
-            var pioneer = await _repository.GetByIdAsync(getPioneerDto.Id);
-            var pioneerDtoObj = _mapper.Map<PioneersDto>(pioneer);
+            var pioneerDtoObj = await _pioneersService.GetPioneerAsync(getPioneerDto);
 
             return Ok(pioneerDtoObj);
         }
@@ -59,23 +42,15 @@ namespace ifst.API.ifst.API.Controllers
         [HttpGet("GetAllPioneers")]
         public async Task<IActionResult> GetAllPioneers([FromQuery] GetAllPioneersDto getPioneersDto)
         {
-            var totalPioneers = await _repository.GetAllAsync();
-            var totalCount = totalPioneers.Count();
-
-            var paginatedPioneers = await _repository.GetAllPaginated(null, pageNumber: getPioneersDto.Page,
-                pageSize: getPioneersDto.PageSize);
-            
-            var dtoResult = new PaginatedResult<PioneersDto>
-            {
-                
-                Items = paginatedPioneers.Items.Select(p => _mapper.Map<PioneersDto>(p)).ToList(),
-                TotalCount = paginatedPioneers.TotalCount,
-                PageNumber = paginatedPioneers.PageNumber,
-                PageSize = paginatedPioneers.PageSize
-            };
-
-
+            var dtoResult = await _pioneersService.GetAllPioneersAsync(getPioneersDto);
             return Ok(dtoResult);
+        }
+
+        [HttpDelete("RemovePioneer/{getPioneerDto.Id}")]
+        public async Task<IActionResult> RemovePioneer([FromRoute] GetPioneersDto getPioneerDto)
+        {
+            await _pioneersService.RemovePioneerAsync(getPioneerDto);
+            return Ok("Pioneer Has Been Deleted");
         }
     }
 }
