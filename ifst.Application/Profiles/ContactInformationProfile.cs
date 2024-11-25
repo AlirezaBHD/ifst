8e2bf4795899c3145e08ce30072ab8ca9a64397f
Smp <alirezaqxr@gmail.com>
2024-11-25 11:23:54 +0330

    Add ContactInformationProfile

---
+++ ifst.Application/Profiles/ContactInformationProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using ifst.API.ifst.Application.DTOs;
+using ifst.API.ifst.Domain.Entities;
+
+namespace ifst.API.ifst.Application.Profiles;
+
+public class ContactInformationProfile : Profile
+{
+    public ContactInformationProfile()
+    {
+        CreateMap<ContactInformationDto, ContactInformation>();
+        CreateMap<ContactInformation, ContactInformationDto>();
+    }
+}
