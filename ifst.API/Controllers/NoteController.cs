ef56e6201de3ace5f142d5d4db9f106f7960dfc9
Smp <alirezaqxr@gmail.com>
2024-11-30 15:12:56 +0330

    Add Post Note to Conteroller

---
+++ ifst.API/Controllers/NoteController.cs
@@ -0,0 +1,28 @@
+﻿using ifst.API.ifst.Application.DTOs;
+using ifst.API.ifst.Application.Interfaces.ServiceInterfaces;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ifst.API.ifst.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class NoteController: ControllerBase
+{
+    private readonly INoteService _noteService;
+    
+    public NoteController(INoteService noteService)
+    {
+        _noteService = noteService;
+    }
+    
+    // [HttpGet]
+
+    [HttpPost("AddNote")]
+    public async Task<IActionResult> AddNote(AddNoteDto noteDto)
+    {
+        await _noteService.AddNote(noteDto);
+        // return Created()
+        return Ok(".یاداشت افزوده شد");
+    }
+}
