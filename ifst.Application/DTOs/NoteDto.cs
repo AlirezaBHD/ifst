4402117808908246c41185ae227273d4c32ac4f7
Smp <alirezaqxr@gmail.com>
2024-11-30 15:11:10 +0330

    Add NoteDto and AddNoteDto

---
+++ ifst.Application/DTOs/NoteDto.cs
@@ -0,0 +1,23 @@
+﻿namespace ifst.API.ifst.Application.DTOs;
+
+public class NoteDto
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string ImagePath { get; set; }
+
+    public string Summery { get; set; }
+    
+    public string Body { get; set; }
+    public DateTime Date { get; set; }
+}
+
+public class AddNoteDto
+{
+    public string Title { get; set; }
+    public IFormFile Image { get; set; }
+
+    public string Summery { get; set; }
+    
+    public string Body { get; set; }
+}
