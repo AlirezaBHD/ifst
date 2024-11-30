a8eba366ec546920fac6cd6479f983ef94cc274b
Smp <alirezaqxr@gmail.com>
2024-11-30 13:26:04 +0330

    Change body type HTMLContent to String

--- ifst.Domain/Entities/Note.cs
+++ ifst.Domain/Entities/Note.cs
@@ -15,6 +15,6 @@ public class Note
     public int Id { get; set; }
     [MaxLength(50)] [Required] public string Title { get; set; }
     [MaxLength(150)] public string? Summery { get; set; }
-    public HtmlString Body { get; set; }
+    public string Body { get; set; }
     public DateTime Date { get; private set; }
 }
