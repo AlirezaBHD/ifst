c8c6c472b7c1f272b3466df3be89bc72e4225660
Smp <alirezaqxr@gmail.com>
2024-11-30 13:22:12 +0330

    Config Entity in Application Db Context

--- ifst.Infrastructure/Data/ApplicationDbContext.cs
+++ ifst.Infrastructure/Data/ApplicationDbContext.cs
@@ -56,5 +56,15 @@ public class ApplicationDbContext : DbContext
             entity.Property(ci => ci.ImagePath);
             entity.Property(ci => ci.FilePath);
         });
+
+        modelBuilder.Entity<Note>(entity =>
+        {
+            entity.HasKey(n => n.Id);
+            entity.Property(n => n.Title);
+            entity.Property(n => n.Summery);
+            entity.Property(n => n.Body);
+            entity.Property(n => n.Date);
+        });
+        
     }
 }
