0ec2d9d380062484a3ca05d6cf915f621f29af85
Smp <alirezaqxr@gmail.com>
2024-11-30 15:11:27 +0330

    create Profile for Note

---
+++ ifst.Application/Profiles/NoteProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using ifst.API.ifst.Application.DTOs;
+using ifst.API.ifst.Domain.Entities;
+
+namespace ifst.API.ifst.Application.Profiles;
+
+public class NoteProfile: Profile
+{
+    public NoteProfile()
+    {
+        CreateMap<AddNoteDto, Note>();
+        // CreateMap<Note, AddNoteDto>();
+    }
+}
