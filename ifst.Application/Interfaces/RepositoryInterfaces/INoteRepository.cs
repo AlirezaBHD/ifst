9d40bab2f0780e3b2f1b006bc7f86392108b9840
Smp <alirezaqxr@gmail.com>
2024-11-30 15:10:33 +0330

    Add Note Repository

---
+++ ifst.Application/Interfaces/RepositoryInterfaces/INoteRepository.cs
@@ -0,0 +1,9 @@
+﻿using ifst.API.ifst.Domain.Entities;
+
+namespace ifst.API.ifst.Application.Interfaces;
+
+public interface INoteRepository :IRepository<Note>
+{
+    
+    
+}
---
+++ ifst.Infrastructure/Data/Repository/NoteRepository.cs
@@ -0,0 +1,14 @@
+﻿using ifst.API.ifst.Application.Interfaces;
+using ifst.API.ifst.Domain.Entities;
+
+namespace ifst.API.ifst.Infrastructure.Data.Repository;
+
+public class NoteRepository: Repository<Note>, INoteRepository
+{
+    private readonly ApplicationDbContext _context;
+
+    public NoteRepository(ApplicationDbContext context) : base(context)
+    {
+        _context = context;
+    }
+}
