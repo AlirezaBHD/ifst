186a8d8711bad6cd4b309f8d3f662b875a2c1c37
Smp <alirezaqxr@gmail.com>
2024-11-30 13:21:44 +0330

    Create Entity

---
+++ ifst.Domain/Entities/Note.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using ifst.API.ifst.Domain.Common;
+using Microsoft.AspNetCore.Html;
+
+namespace ifst.API.ifst.Domain.Entities;
+
+[DisplayName("یاداشت")]
+public class Note
+{
+    public Note()
+    {
+        Date = DateTime.Now;
+    }
+
+    public int Id { get; set; }
+    [MaxLength(50)] [Required] public string Title { get; set; }
+    [MaxLength(150)] public string? Summery { get; set; }
+    public HtmlString Body { get; set; }
+    public DateTime Date { get; private set; }
+}
