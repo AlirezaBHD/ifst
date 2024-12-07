8395749f56b61ae7425105ebae6276ea1bfeefe7
Smp <alirezaqxr@gmail.com>
2024-12-07 15:14:46 +0330

    Create Project Repository

---
+++ ifst.Infrastructure/Data/Repository/ProjectRepository.cs
@@ -0,0 +1,13 @@
+﻿using ifst.API.ifst.Application.Interfaces;
+using ifst.API.ifst.Domain.Entities;
+
+namespace ifst.API.ifst.Infrastructure.Data.Repository;
+
+public class ProjectRepository: Repository<Project> , IProjectRepository
+{
+    private readonly ApplicationDbContext _context;
+    public ProjectRepository(ApplicationDbContext context) : base(context)
+    {
+        _context = context;
+    }
+}
