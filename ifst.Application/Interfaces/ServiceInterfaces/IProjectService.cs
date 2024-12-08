9ab186a5e888cecf793538347f973c08a4c5a079
Smp <alirezaqxr@gmail.com>
2024-12-08 10:36:53 +0330

    Add Project Service interface

---
+++ ifst.Application/Interfaces/ServiceInterfaces/IProjectService.cs
@@ -0,0 +1,17 @@
+﻿using ifst.API.ifst.Application.DTOs;
+using ifst.API.ifst.Application.Extensions;
+
+namespace ifst.API.ifst.Application.Interfaces.ServiceInterfaces;
+
+public interface IProjectService
+{
+    Task AddProjectAsync(GetObjectByIdDto institute, CreateProjectDto projectDto);
+
+    // Task<IEnumerable<Newsletter>> GetProjectsAsync(FilterAndSortPaginatedOptions options);
+    //
+    // Task<NewsletterDto> GetProject (GetObjectByIdDto newsletter);
+    //
+    // Task<Newsletter> UpdateProject(int id, PatchNewsletterDto newsletterDto);
+    //
+    // Task DeleteProject(GetObjectByIdDto newsletter);
+}
