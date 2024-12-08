bb9c147de195de25fc569b882191bfb23b380808
Smp <alirezaqxr@gmail.com>
2024-12-08 11:46:22 +0330

    Fixe Display name of Institute

--- ifst.Domain/Entities/Institute.cs
+++ ifst.Domain/Entities/Institute.cs
@@ -3,7 +3,7 @@ using System.ComponentModel.DataAnnotations;
 
 namespace ifst.API.ifst.Domain.Entities;
 
-[DisplayName("آلبوم")]
+[DisplayName("بنیاد")]
 public class Institute
 {
     public int Id { get; set; }
