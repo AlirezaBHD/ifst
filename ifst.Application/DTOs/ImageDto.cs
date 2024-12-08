c979071388482258e46af41109865767fff571a1
Smp <alirezaqxr@gmail.com>
2024-12-08 12:02:57 +0330

    Fixe ImageDto

--- ifst.Application/DTOs/ImageDto.cs
+++ ifst.Application/DTOs/ImageDto.cs
@@ -17,7 +17,6 @@ public class ImageDto
     public int Id { get; set; }
     public string Path { get; set; }
     public string Description { get; set; }
-    public int AlbumId { get; set; }
     
 }
 
