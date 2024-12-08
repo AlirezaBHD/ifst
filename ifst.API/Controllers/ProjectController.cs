55fb9b74fee87c3effdc5d45d6de1791d3a648a5
Smp <alirezaqxr@gmail.com>
2024-12-08 10:39:25 +0330

    Create Post AddProject API

---
+++ ifst.API/Controllers/ProjectController.cs
@@ -0,0 +1,25 @@
+﻿using ifst.API.ifst.Application.DTOs;
+using ifst.API.ifst.Application.Interfaces.ServiceInterfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ifst.API.ifst.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+
+public class ProjectController: ControllerBase
+{
+    private readonly IProjectService _projectService;
+    
+    public ProjectController(IGeneralServices generalServices, IProjectService projectService)
+    {
+        _projectService = projectService;
+    }
+
+    [HttpPost("AddProject/{institute.Id}")]
+    public async Task<IActionResult> AddProject([FromRoute]GetObjectByIdDto institute,[FromForm] CreateProjectDto project)
+    {
+        await _projectService.AddProjectAsync(institute, project);
+        return Ok(".پروژه با موفقیت اضافه شد");
+    }
+}
