45d2a4e40609ff557de3e89215bb69919d2d9540
Smp <alirezaqxr@gmail.com>
2024-12-11 09:13:14 +0330

    Complete UpdateProject service

--- ifst.Application/Services/ProjectService.cs
+++ ifst.Application/Services/ProjectService.cs
@@ -1,8 +1,11 @@
 ﻿using AutoMapper;
+using FluentValidation;
+using FluentValidation.Results;
 using ifst.API.ifst.Application.DTOs;
 using ifst.API.ifst.Application.Interfaces;
 using ifst.API.ifst.Application.Interfaces.ServiceInterfaces;
 using ifst.API.ifst.Domain.Entities;
+using ifst.API.ifst.Domain.ValueObjects;
 using ifst.API.ifst.Infrastructure.FileManagement;
 
 namespace ifst.API.ifst.Application.Services;
@@ -14,8 +17,10 @@ public class ProjectService : IProjectService
     private readonly IMapper _mapper;
     private readonly IGeneralServices _generalServices;
     private readonly FileService _fileService;
-    public ProjectService(IProjectRepository projectRepository, IGeneralServices generalServices, FileService fileService, IMapper mapper, IInstituteRepository instituteRepository)
+    private readonly IGenericService<Project> _genrricService;
+    public ProjectService(IGenericService<Project> genericService,IProjectRepository projectRepository, IGeneralServices generalServices, FileService fileService, IMapper mapper, IInstituteRepository instituteRepository)
     {
+        _genrricService = genericService;
         _projectRepository = projectRepository;
         _generalServices = generalServices;
         _fileService = fileService;
@@ -39,7 +44,7 @@ public class ProjectService : IProjectService
 
     public async Task<ProjectDetailDto> GetProject(GetObjectByIdDto projectDto)
     {
-        var project = await _projectRepository.GetByIdWithIncludesAsync(projectDto.Id, project => project.Institute);
+        var project = await _projectRepository.GetByIdWithIncludesAsync(projectDto.Id, condition:p => p.Status == ProjectStatus.Approved, includes:project => project.Institute);
         var projectObj = _mapper.Map<ProjectDetailDto>(project);
         return projectObj;
     }
@@ -49,5 +54,16 @@ public class ProjectService : IProjectService
         var project = await _projectRepository.GetByIdAsync(projectDto.Id);
         _projectRepository.Remove(project);
         await _generalServices.SaveAsync();
+    }
+
+    public async Task UpdateProject(GetObjectByIdDto projectDto, InstituteUpdateProjectDto updateProjectDto)
+    {
+        var projectDtoObj = await _projectRepository.GetByIdAsync(projectDto.Id);
+        var files = new Dictionary<string, IFormFile>
+            {
+                { nameof(projectDtoObj.ImagePath),updateProjectDto.ImageFile }
+            };
+
+        await _genrricService.UpdateEntityAsync(projectDtoObj,updateProjectDto,files);
     }
 }
