676985ada3f5661b54a197b77c0894c8ec2b7d70
Smp <alirezaqxr@gmail.com>
2024-12-11 09:09:16 +0330

    Create Generic Service for Update Entity

---
+++ ifst.Application/Interfaces/ServiceInterfaces/IGenericService.cs
@@ -0,0 +1,12 @@
+﻿namespace ifst.API.ifst.Application.Interfaces.ServiceInterfaces;
+
+public interface IGenericService<TEntity> where TEntity : class
+{
+    Task<TEntity> AddEntityAsync<TDto>(TDto dto);
+
+    Task UpdateEntityAsync<TDto>(
+        TEntity entity,
+        TDto updateDto,
+        Dictionary<string,IFormFile> files)
+        where TDto : class;
+}
