732d5789dbb66b253feeeffa4d22a0673195a7c6
Smp <alirezaqxr@gmail.com>
2024-12-11 09:11:21 +0330

    Add UpdateProject service to Service

--- ifst.Application/Interfaces/ServiceInterfaces/IProjectService.cs
+++ ifst.Application/Interfaces/ServiceInterfaces/IProjectService.cs
@@ -10,8 +10,8 @@ public interface IProjectService
     // Task<IEnumerable<Newsletter>> GetProjectsAsync(FilterAndSortPaginatedOptions options);
     //
     Task<ProjectDetailDto> GetProject (GetObjectByIdDto projectDto);
-    //
-    // Task<Newsletter> UpdateProject(int id, PatchNewsletterDto newsletterDto);
-    //
+    
+    Task UpdateProject(GetObjectByIdDto projectDto, InstituteUpdateProjectDto newsletterDto);
+    
     Task DeleteProject(GetObjectByIdDto projectDto);
 }
