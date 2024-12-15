261b1d2a06cde49b8e61cd203a3a048c8c13cacc
Smp <alirezaqxr@gmail.com>
2024-12-15 10:03:42 +0330

    Add Institute -> InstituteDto Profile

--- ifst.Application/Profiles/InstituteProfile.cs
+++ ifst.Application/Profiles/InstituteProfile.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using ifst.API.ifst.Application.DTOs;
 using ifst.API.ifst.Domain.Entities;
+using ifst.API.ifst.Domain.ValueObjects;
 
 namespace ifst.API.ifst.Application.Profiles;
 
@@ -9,7 +10,9 @@ public class InstituteProfile : Profile
     public InstituteProfile()
     {
         CreateMap<CreateInstituteDto, Institute>();
-        CreateMap<Institute, InstituteDto>();
+        CreateMap<Institute, InstituteDto>()
+            .ForMember(dest => dest.Projects, opt => opt.MapFrom(src => 
+                src.Projects.Where(p => p.Status == ProjectStatus.Approved)));
         CreateMap<Institute, ListedInstitutesDto>();
         CreateMap<Institute, MainListedInstitutesDto>();
         CreateMap<Institute, PatchInstitutesDto>();
