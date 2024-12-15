4c6856ce23469c249b473486a458933f46891fd9
Smp <alirezaqxr@gmail.com>
2024-12-15 10:01:24 +0330

    Add GetAllInstitutesAndProjects to repository

--- ifst.Application/Interfaces/RepositoryInterfaces/IInstituteRepository.cs
+++ ifst.Application/Interfaces/RepositoryInterfaces/IInstituteRepository.cs
@@ -1,8 +1,9 @@
-﻿using ifst.API.ifst.Domain.Entities;
+﻿using ifst.API.ifst.Application.DTOs;
+using ifst.API.ifst.Domain.Entities;
 
 namespace ifst.API.ifst.Application.Interfaces;
 
 public interface IInstituteRepository: IRepository<Institute>
 {
-    
+    Task<IEnumerable<InstituteDto>> GetAllInstitutesAndProjects();
 }
--- ifst.Infrastructure/Data/Repository/InstituteRepository.cs
+++ ifst.Infrastructure/Data/Repository/InstituteRepository.cs
@@ -1,20 +1,35 @@
-﻿using AutoMapper;
+﻿using System.Linq.Expressions;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using ifst.API.ifst.Application.DTOs;
 using ifst.API.ifst.Application.Interfaces;
 using ifst.API.ifst.Domain.Entities;
+using ifst.API.ifst.Domain.ValueObjects;
+using Microsoft.EntityFrameworkCore;
 
 namespace ifst.API.ifst.Infrastructure.Data.Repository;
 
-
-
-public class InstituteRepository : Repository<Institute>,IInstituteRepository
+public class InstituteRepository : Repository<Institute>, IInstituteRepository
 {
     private readonly ApplicationDbContext _context;
     private readonly IMapper _mapper;
 
-    
+
     public InstituteRepository(ApplicationDbContext context, IMapper mapper) : base(context, mapper)
     {
         _context = context;
         _mapper = mapper;
+    }
+
+
+    public async Task<IEnumerable<InstituteDto>> GetAllInstitutesAndProjects()
+    {
+
+        var institutes = await _context.Institute
+            .Where(i => i.Confirmed)
+            .ProjectTo<InstituteDto>(_mapper.ConfigurationProvider)
+            .ToListAsync();
+
+        return institutes;
     }
 }
