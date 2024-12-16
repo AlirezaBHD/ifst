90a6feed21d7b4fdbb18dba6bbaf79078db84b70
Smp <alirezaqxr@gmail.com>
2024-12-16 10:11:20 +0330

    Create News Entity

---
+++ ifst.Domain/Entities/News.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace ifst.API.ifst.Domain.Entities;
+
+[DisplayName("خبر")]
+public class News
+{
+    public News()
+    {
+        Date = DateTime.Now;
+    }
+
+    public int Id { get; set; }
+    [MaxLength(50)] [Required] public string Title { get; set; }
+    public string ImagePath { get; set; }
+    [MaxLength(150)] public string? Summery { get; set; }
+    public string Body { get; set; }
+    public DateTime Date { get; private set; }
+}
