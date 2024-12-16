5d6588d8a535f0f3f6801513493249b90be8d692
Smp <alirezaqxr@gmail.com>
2024-12-16 10:19:48 +0330

    Create Profile for News

---
+++ ifst.Application/Profiles/NewsProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using ifst.API.ifst.Application.DTOs;
+using ifst.API.ifst.Application.DTOs.NewsDto;
+using ifst.API.ifst.Domain.Entities;
+
+namespace ifst.API.ifst.Application.Profiles;
+
+public class NewsProfile : Profile
+{
+    public NewsProfile()
+    {
+        CreateMap<CreateNewsDto, News>();
+        CreateMap<News, NewsDto>();
+        CreateMap<News, NewsDetailDto>();
+        CreateMap<UpdateNewsDto, News>();
+        CreateMap<News, ListedNewsDto>();
+    }
+}
