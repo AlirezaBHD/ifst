85d5a93dd936ac00f14042a005e1fbb02b95e554
Smp <alirezaqxr@gmail.com>
2024-12-16 10:46:44 +0330

    Create AparatVideo Entity

---
+++ ifst.Domain/Entities/AparatVideo.cs
@@ -0,0 +1,10 @@
+﻿namespace ifst.API.ifst.Domain.Entities;
+
+public class AparatVideo
+{
+    public int Id { get; set; }
+
+    public string Title { get; set; }
+
+    public string VideoScript { get; set; }
+}
