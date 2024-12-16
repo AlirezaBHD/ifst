2b5d1cf04af6d5ee972a365977f626e497f10e89
Smp <alirezaqxr@gmail.com>
2024-12-16 10:57:28 +0330

    Create ApatatVideoRepository

---
+++ ifst.Infrastructure/Data/Repository/AparatVideoRepository.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using ifst.API.ifst.Application.Interfaces;
+using ifst.API.ifst.Domain.Entities;
+
+namespace ifst.API.ifst.Infrastructure.Data.Repository;
+
+public class AparatVideoRepository: Repository<AparatVideo>, IAparatVideoRepository
+{
+    private readonly ApplicationDbContext _context;
+    private readonly IMapper _mapper;
+
+    public AparatVideoRepository(ApplicationDbContext context, IMapper mapper) : base(context , mapper)
+    {
+        _context = context;
+        _mapper = mapper;
+    }
+}
