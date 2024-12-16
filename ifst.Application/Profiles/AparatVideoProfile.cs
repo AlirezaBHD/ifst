dfe6a97e16b03c4a0ffe1831d2dcf9947d359016
Smp <alirezaqxr@gmail.com>
2024-12-16 11:42:29 +0330

    CreateAparatVideoDto -> AparatVideo Profile

---
+++ ifst.Application/Profiles/AparatVideoProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using ifst.API.ifst.Application.DTOs.AparatVideoDto;
+using ifst.API.ifst.Domain.Entities;
+
+namespace ifst.API.ifst.Application.Profiles;
+
+public class AparatVideoProfile : Profile
+{
+    public AparatVideoProfile()
+    {
+        CreateMap<CreateAparatVideoDto, AparatVideo>();
+
+    }
+}
