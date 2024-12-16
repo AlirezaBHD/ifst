44ee8ddcaff4091e4dad577e0db48de026684885
Smp <alirezaqxr@gmail.com>
2024-12-16 12:01:29 +0330

    interface for CreateAparatVideoAsync

--- ifst.Application/Interfaces/ServiceInterfaces/IAparatVideoService.cs
+++ ifst.Application/Interfaces/ServiceInterfaces/IAparatVideoService.cs
@@ -1,6 +1,8 @@
-﻿namespace ifst.API.ifst.Application.Interfaces.ServiceInterfaces;
+﻿using ifst.API.ifst.Application.DTOs.AparatVideoDto;
+
+namespace ifst.API.ifst.Application.Interfaces.ServiceInterfaces;
 
 public interface IAparatVideoService
 {
-    
+    Task<AparatVideoDto> CreateAparatVideoAsync(CreateAparatVideoDto aparatVideo);
 }
