0754069f0dc7954d28260bb2b6fbaa8538af3e35
Smp <alirezaqxr@gmail.com>
2024-12-16 11:30:17 +0330

    Validator for CreateAparatVideoDto

---
+++ ifst.Application/FluentValidation/AparatVideoValidator/CreateAparatVideoValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+using ifst.API.ifst.Application.DTOs.AparatVideoDto;
+using ifst.API.ifst.Application.FluentValidation.ValidationExtensions;
+
+namespace JsonPatchSample.ifst.Application.FluentValidation;
+
+public class CreateAparatVideoValidator : AbstractValidator<CreateAparatVideoDto>
+{
+    public CreateAparatVideoValidator()
+    {
+        RuleFor(av => av.Title).CommonStringRules(3, 50, "عنوان");
+        RuleFor(av => av.VideoScript).CommonTHMLRules("اسکیریپت ویدیو آپارات");
+    }
+}
