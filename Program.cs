6cea859ac3cbf6041eac9c4415dcf4bab48ec786
Smp <alirezaqxr@gmail.com>
2024-12-16 15:01:22 +0330

    Revert "Add Scope AparatVideo Repository to Program.cs"
    This reverts commit 4f2a7ab3bc6cd583291cca5c77d776d769441ab2.

--- Program.cs
+++ Program.cs
@@ -42,7 +42,6 @@ builder.Services.AddScoped<IInstituteRepository, InstituteRepository>();
 builder.Services.AddScoped<IProjectRepository, ProjectRepository>();
 builder.Services.AddScoped<IPublicImageRepository, PublicImageRepository>();
 builder.Services.AddScoped<INewsRepository, NewsRepository>();
-builder.Services.AddScoped<IAparatVideoRepository, AparatVideoRepository>();
 //
 builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
 //
