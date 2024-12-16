4014b5ebc04925358ff5d6b462b598ffa5c3d82d
Smp <alirezaqxr@gmail.com>
2024-12-16 10:17:25 +0330

    Create News Repository

---
+++ ifst.Infrastructure/Data/Repository/NewsRepository.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using ifst.API.ifst.Application.Interfaces;
+using ifst.API.ifst.Domain.Entities;
+
+namespace ifst.API.ifst.Infrastructure.Data.Repository;
+
+public class NewsRepository: Repository<News>, INewsRepository
+{
+    private readonly ApplicationDbContext _context;
+    private readonly IMapper _mapper;
+
+    public NewsRepository(ApplicationDbContext context, IMapper mapper) : base(context, mapper)
+    {
+        _context = context;
+        _mapper = mapper;
+    }
+}
