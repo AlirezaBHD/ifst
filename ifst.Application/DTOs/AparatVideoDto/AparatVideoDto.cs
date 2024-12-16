547a0d2b3989451326bbddab4f32fbb352e152ad
Smp <alirezaqxr@gmail.com>
2024-12-16 11:54:07 +0330

    Create AparatVideoDto

---
+++ ifst.Application/DTOs/AparatVideoDto/AparatVideoDto.cs
@@ -0,0 +1,10 @@
+﻿namespace ifst.API.ifst.Application.DTOs.AparatVideoDto;
+
+public class AparatVideoDto
+{
+    public int Id { get; set; }
+    
+    public string Title { get; set; }
+    
+    public string VideoScript { get; set; }
+}
