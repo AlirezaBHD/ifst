735f066e0c8e4c43d23a24ad239a18930d5b973b
Smp <alirezaqxr@gmail.com>
2024-12-16 11:44:41 +0330

    Create AparatVideo Controller

---
+++ ifst.API/Controllers/AparatVideoController.cs
@@ -0,0 +1,19 @@
+﻿using ifst.API.ifst.Application.Interfaces.ServiceInterfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ifst.API.ifst.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class AparatVideoController : ControllerBase
+{
+    #region Injection
+
+    private readonly IAparatVideoService _aparatVideoService;
+
+    public AparatVideoController(IAparatVideoService aparatVideoService)
+    {
+        _aparatVideoService = aparatVideoService;
+    }
+    #endregion
+}
