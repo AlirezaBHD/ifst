daca4fff432c2ea586e8dc8a625769ab7db411e0
Smp <alirezaqxr@gmail.com>
2024-12-16 12:13:04 +0330

    Add CreateAparatVideoAsync Service interface

--- ifst.Application/Interfaces/ServiceInterfaces/IAparatVideoService.cs
+++ ifst.Application/Interfaces/ServiceInterfaces/IAparatVideoService.cs
@@ -1,8 +1,11 @@
-﻿using ifst.API.ifst.Application.DTOs.AparatVideoDto;
+﻿using ifst.API.ifst.Application.DTOs;
+using ifst.API.ifst.Application.DTOs.AparatVideoDto;
 
 namespace ifst.API.ifst.Application.Interfaces.ServiceInterfaces;
 
 public interface IAparatVideoService
 {
     Task<AparatVideoDto> CreateAparatVideoAsync(CreateAparatVideoDto aparatVideo);
+    
+    Task<AparatVideoDto> GetAparatVideoAsync(int id);
 }
