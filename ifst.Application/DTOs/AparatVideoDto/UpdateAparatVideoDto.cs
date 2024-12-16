8b83b4205c7cda8c9e33e029d583edfec139fd6c
Smp <alirezaqxr@gmail.com>
2024-12-16 13:00:16 +0330

    Fixe UpdateAparatVideoDto

---
+++ ifst.Application/DTOs/AparatVideoDto/UpdateAparatVideoDto.cs
@@ -0,0 +1,8 @@
+﻿namespace ifst.API.ifst.Application.DTOs.AparatVideoDto;
+
+public class UpdateAparatVideoDto
+{
+    public string? Title { get; set; }
+    
+    public string VideoScript { get; set; }
+}
