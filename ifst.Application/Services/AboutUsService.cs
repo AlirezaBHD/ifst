f97fa4a7efc877f0f86e27bf39c6dfb22e088079
Smp <alirezaqxr@gmail.com>
2024-12-17 10:48:18 +0330

    Create AboutUs Service

---
+++ ifst.Application/Services/AboutUsService.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using ifst.API.ifst.Application.Interfaces;
+using ifst.API.ifst.Application.Interfaces.ServiceInterfaces;
+using ifst.API.ifst.Domain.Entities;
+using ifst.API.ifst.Infrastructure.FileManagement;
+
+namespace ifst.API.ifst.Application.Services;
+
+public class AboutUsService : IAboutUsService
+{
+    private readonly IAboutUsRepository _aboutUsRepository;
+    private readonly IMapper _mapper;
+    private readonly IGeneralServices<AboutUs> _generalServices;
+    private readonly FileService _fileService;
+
+    public AboutUsService(IAboutUsRepository aboutUsRepository, IGeneralServices<AboutUs> generalServices,
+        FileService fileService, IMapper mapper)
+    {
+        _aboutUsRepository = aboutUsRepository;
+        _generalServices = generalServices;
+        _fileService = fileService;
+        _mapper = mapper;
+    }
+}
