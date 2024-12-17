5d897512045c4a99521a7d900b0f5741d3625374
Smp <alirezaqxr@gmail.com>
2024-12-17 15:16:24 +0330

    Create Dto for AboutUs

---
+++ ifst.Application/DTOs/AboutUs/AboutUsDto.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ifst.API.ifst.Application.DTOs;
+
+public class AboutUsDto
+{
+    public int Id { get; set; }
+    [Required] public string Introduction { get; set; }
+    [Required] public string Statutes { get; set; }
+    [Required] public string ActivityLicense { get; set; }
+    [Required] public string FoundingBoard { get; set; }
+    [Required] public string BoardOfTrustees { get; set; }
+    [Required] public string BoardOfDirectors { get; set; }
+    [Required] public string CEO { get; set; }
+    [Required] public string CommitteesAndWorkingGroups { get; set; }
+    [Required] public string Archive { get; set; }
+    [Required] public string Reports { get; set; }
+}
