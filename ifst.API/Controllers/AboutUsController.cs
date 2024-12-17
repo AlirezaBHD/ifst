af9fd4e68f10e8924ec85ef2920b4450c2d1921a
Smp <alirezaqxr@gmail.com>
2024-12-17 15:27:27 +0330

    Create Get AboutUs Object

---
+++ ifst.API/Controllers/AboutUsController.cs
@@ -0,0 +1,27 @@
+﻿using ifst.API.ifst.Application.Interfaces.ServiceInterfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ifst.API.ifst.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class AboutUsController : ControllerBase
+{
+    #region Injection
+
+    private readonly IAboutUsService _aboutUsService;
+
+    public AboutUsController(IAboutUsService aboutUsService)
+    {
+        _aboutUsService = aboutUsService;
+    }
+
+    #endregion
+
+    [HttpGet("AboutUs")]
+    public async Task<IActionResult> GetAboutUsAsync()
+    {
+        var result = await _aboutUsService.GetAboutUsAsync();
+        return Ok(result);
+    }
+}
