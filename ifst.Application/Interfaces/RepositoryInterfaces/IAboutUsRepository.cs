29fe04535cacbd9ac26e9c9cd93209f91e2bcc05
Smp <alirezaqxr@gmail.com>
2024-12-17 15:19:14 +0330

    Create AboutUs firstOrDefault interface

--- ifst.Application/Interfaces/RepositoryInterfaces/IAboutUsRepository.cs
+++ ifst.Application/Interfaces/RepositoryInterfaces/IAboutUsRepository.cs
@@ -1,8 +1,9 @@
-﻿using ifst.API.ifst.Domain.Entities;
+﻿using ifst.API.ifst.Application.DTOs;
+using ifst.API.ifst.Domain.Entities;
 
 namespace ifst.API.ifst.Application.Interfaces;
 
 public interface IAboutUsRepository : IRepository<AboutUs>
 {
-    
+    Task<AboutUsDto> AboutUsObject();
 }
