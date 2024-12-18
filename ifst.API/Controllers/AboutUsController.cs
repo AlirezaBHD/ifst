e362e096e5f0f5e85c39a1b35f4e10002b215ce9
Smp <alirezaqxr@gmail.com>
2024-12-18 09:30:24 +0330

    Create Put PutAboutUsAsync in AboutUs Controller

--- ifst.API/Controllers/AboutUsController.cs
+++ ifst.API/Controllers/AboutUsController.cs
@@ -1,4 +1,5 @@
-﻿using ifst.API.ifst.Application.Interfaces.ServiceInterfaces;
+﻿using ifst.API.ifst.Application.DTOs;
+using ifst.API.ifst.Application.Interfaces.ServiceInterfaces;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ifst.API.ifst.API.Controllers;
@@ -24,4 +25,13 @@ public class AboutUsController : ControllerBase
         var result = await _aboutUsService.GetAboutUsAsync();
         return Ok(result);
     }
+
+    [HttpPut("AboutUs/Put")]
+    public async Task<IActionResult> PutAboutUsAsync([FromBody] CreateAboutUsDto aboutUs)
+    {
+        await _aboutUsService.PutAboutUsAsync(aboutUs);
+        return Ok(".صفحه درباره ما با موفقیت ویرایش داده شد");
+    }
+    
+    
 }
