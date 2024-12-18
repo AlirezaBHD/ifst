f1bd8b6b5162d68a26638b846ddd8c80b1bb35c2
Smp <alirezaqxr@gmail.com>
2024-12-18 09:29:45 +0330

    Create Profile for AboutUs Dtos

---
+++ ifst.Application/Profiles/AboutUsProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using ifst.API.ifst.Application.DTOs;
+using ifst.API.ifst.Domain.Entities;
+
+namespace ifst.API.ifst.Application.Profiles;
+
+public class AboutUsProfile : Profile
+{
+    public AboutUsProfile()
+    {
+        CreateMap<CreateAboutUsDto, AboutUs>();
+        CreateMap<AboutUs, AboutUsDto>();
+        CreateMap<AboutUsDto, AboutUs>();
+    }
+}
