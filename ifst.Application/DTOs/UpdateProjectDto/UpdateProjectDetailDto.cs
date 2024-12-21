68e530e81399e7d19c1178bd3648faf9e9afee05
Smp <alirezaqxr@gmail.com>
2024-12-21 09:16:30 +0330

    Create UpdateProjectDetailDto

---
+++ ifst.Application/DTOs/UpdateProjectDto/UpdateProjectDetailDto.cs
@@ -0,0 +1,9 @@
+﻿namespace ifst.API.ifst.Application.DTOs.UpdateProjectDto;
+
+public class UpdateProjectDetailDto
+{
+    public int Id { get; set; }
+    public string? Title { get; set; }
+    public string? Description { get; set; }
+    public DateTime Date { get; set; }
+}
