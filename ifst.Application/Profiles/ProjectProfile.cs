1177e2920024487b10c3f641e7d7724b55f3611a
Smp <alirezaqxr@gmail.com>
2024-12-21 09:19:54 +0330

    Add UpdateProject to Project -> ProjectDetailDto map

--- ifst.Application/Profiles/ProjectProfile.cs
+++ ifst.Application/Profiles/ProjectProfile.cs
@@ -6,16 +6,17 @@ namespace ifst.API.ifst.Application.Profiles;
 
 public class ProjectProfile : Profile
 {
-
     public ProjectProfile()
     {
-
         CreateMap<CreateProjectDto, Project>();
         CreateMap<Project, ProjectListDto>();
-        CreateMap<Project, ProjectDetailDto>().ForMember(dest => dest.InstituteName, opt => opt.MapFrom(src => src.Institute.Name));
+        CreateMap<Project, ProjectDetailDto>()
+            .ForMember(dest => dest.InstituteName, opt =>
+                opt.MapFrom(src => src.Institute.Name))
+            .ForMember(dest => dest.Updates, opt =>
+                opt.MapFrom(src => src.Updates.Where(u => u.Accepted)));
         CreateMap<Project, ProjectDto>();
         CreateMap<InstituteUpdateProjectDto, Project>();
-        CreateMap<Project,ProjectsName>();
-
+        CreateMap<Project, ProjectsName>();
     }
 }
