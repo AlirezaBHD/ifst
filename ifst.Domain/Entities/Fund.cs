23bdc0bffd26b8e10b9d44c7f43201fd62c0bb0d
Smp <alirezaqxr@gmail.com>
2024-12-21 17:17:00 +0330

    Create Fund Entity

---
+++ ifst.Domain/Entities/Fund.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using ifst.API.ifst.Domain.Common;
+
+namespace ifst.API.ifst.Domain.Entities;
+
+[DisplayName("صندوق")]
+public class Fund
+{
+    public int Id { get; set; }
+    [Required] public string Name { get; set; }
+    public int? GatheredAmount { get; set; } = 0;
+    public ICollection<Project> Projects { get; set; } = new List<Project>();
+
+}
