2aa2c0b412a786e53fe7331d8739f3ff59dbccb3
Smp <alirezaqxr@gmail.com>
2024-12-21 09:17:35 +0330

    Create UpdateProject -> UpdateProjectDetailDto map profile

---
+++ ifst.Application/Profiles/UpdateProjectProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using ifst.API.ifst.Application.DTOs.UpdateProjectDto;
+using ifst.API.ifst.Domain.Entities;
+
+namespace ifst.API.ifst.Application.Profiles;
+
+public class UpdateProjectProfile : Profile
+{
+    public UpdateProjectProfile()
+    {
+        CreateMap<UpdateProject, UpdateProjectDetailDto>();
+    }
+}
