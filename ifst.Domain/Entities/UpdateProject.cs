a9ee963cba7af4688d17b02f70ebcc710631de0e
Smp <alirezaqxr@gmail.com>
2024-12-21 09:06:30 +0330

    Create UpdateProject entity

---
+++ ifst.Domain/Entities/UpdateProject.cs
@@ -0,0 +1,23 @@
+﻿using ifst.API.ifst.Domain.Common;
+
+namespace ifst.API.ifst.Domain.Entities;
+
+[DisplayName("بروزرسانی پروژه")]
+
+public class UpdateProject
+{
+    public UpdateProject()
+    {
+        Date = DateTime.Now;
+    }
+
+    public int Id { get; set; }
+    public string? Title { get; set; }
+    public string? Description { get; set; }
+    public DateTime Date { get; private set; }
+    public int Progress { get; set; }
+    public bool Accepted { get; set; } = false;
+    
+    public int ProjectId { get; set; }
+    public Project Project { get; set; }
+}
