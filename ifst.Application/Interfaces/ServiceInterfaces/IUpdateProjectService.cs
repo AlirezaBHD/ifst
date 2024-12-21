5ac2a7b980a7fdff42e0965d59d4fea0d9ee2c91
Smp <alirezaqxr@gmail.com>
2024-12-21 10:39:05 +0330

    Add UpdateProjectDetail service interface

--- ifst.Application/Interfaces/ServiceInterfaces/IUpdateProjectService.cs
+++ ifst.Application/Interfaces/ServiceInterfaces/IUpdateProjectService.cs
@@ -1,6 +1,8 @@
-﻿namespace ifst.API.ifst.Application.Interfaces.ServiceInterfaces;
+﻿using ifst.API.ifst.Application.DTOs.UpdateProjectDto;
+
+namespace ifst.API.ifst.Application.Interfaces.ServiceInterfaces;
 
 public interface IUpdateProjectService
 {
-    
+    Task<UpdateProjectDetailDto> UpdateProjectDetail(int id);
 }
