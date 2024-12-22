c9d5a06f742f30922742c8e891eadd209773524f
Smp <alirezaqxr@gmail.com>
2024-12-22 09:28:34 +0330

    Create Fund service

---
+++ ifst.Application/Services/FundService.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using ifst.API.ifst.Application.Interfaces;
+using ifst.API.ifst.Application.Interfaces.ServiceInterfaces;
+using ifst.API.ifst.Domain.Entities;
+using ifst.API.ifst.Infrastructure.FileManagement;
+
+namespace ifst.API.ifst.Application.Services;
+
+public class FundService : IFundService
+{
+    private readonly IFundRepository _fundRepository;
+    private readonly IMapper _mapper;
+    private readonly IGeneralServices<Fund> _generalServices;
+    private readonly FileService _fileService;
+
+    public FundService(IGeneralServices<Fund> generalServices,
+        FileService fileService, IMapper mapper, IFundRepository fundRepository)
+    {
+        _generalServices = generalServices;
+        _fileService = fileService;
+        _mapper = mapper;
+        _fundRepository = fundRepository;
+    }
+}
