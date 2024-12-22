5a5ec4e85ee29a0f0f3cd325d9abcb3275294bce
Smp <alirezaqxr@gmail.com>
2024-12-22 09:27:49 +0330

    Create Fund Repository

---
+++ ifst.Infrastructure/Data/Repository/FundRepository.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using ifst.API.ifst.Application.Interfaces;
+using ifst.API.ifst.Domain.Entities;
+
+namespace ifst.API.ifst.Infrastructure.Data.Repository;
+
+public class FundRepository:Repository<Fund>, IFundRepository
+{
+    private readonly ApplicationDbContext _context;
+    private readonly IMapper _mapper;
+
+    public FundRepository(ApplicationDbContext context, IMapper mapper) : base(context, mapper)
+    {
+        _context = context;
+        _mapper = mapper;
+    }
+}
