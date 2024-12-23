0a20847902cb12e8c8edc93e2adb95cdb862961e
Smp <alirezaqxr@gmail.com>
2024-12-23 16:51:05 +0330

    Add StudentId to Student Entity

--- ifst.Domain/Entities/Student.cs
+++ ifst.Domain/Entities/Student.cs
@@ -7,16 +7,18 @@ namespace ifst.API.ifst.Domain.Entities;
 public class Student
 {
     public int Id { get; set; }
-    [Required] public string Name { get; set; }
-    [Required] public string LastName { get; set; }
+    [MaxLength(50)][Required] public required string Name { get; set; }
+    [MaxLength(50)][Required] public required string LastName { get; set; }
     public string? StudentCardImagePath { get; set; }
     public string? NationalCardImagePath { get; set; }
-    public string? Field { get; set; }
+    [MaxLength(50)]public string? LastUniversity { get; set; }
+    [MaxLength(50)]public string? Field { get; set; }
     public string? Email { get; set; }
     public string? PhoneNumber { get; set; }
-    [Required] public string NationalCode { get; set; }
+    [MaxLength(50)][Required] public required string NationalCode { get; set; }
+    [MaxLength(50)]public string? StudentId { get; set; }
     public DateOnly? GraduationDate { get; set; }
-    public string? YearOfEntry { get; set; }
+    [MaxLength(10)]public string? YearOfEntry { get; set; }
 
     public int InstituteId { get; set; }
     public Institute Institute { get; set; }
