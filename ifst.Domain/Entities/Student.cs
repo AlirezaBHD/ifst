9ac50896d21d733de7ba7b97e35cfbcb0ea823a1
Smp <alirezaqxr@gmail.com>
2024-12-23 15:42:08 +0330

    Create Student Entity

---
+++ ifst.Domain/Entities/Student.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using ifst.API.ifst.Domain.Common;
+
+namespace ifst.API.ifst.Domain.Entities;
+
+[DisplayName("دانشجو")]
+public class Student
+{
+    public int Id { get; set; }
+    [Required] public string Name { get; set; }
+    [Required] public string LastName { get; set; }
+    public string? StudentCardImagePath { get; set; }
+    public string? NationalCardImagePath { get; set; }
+    public string? Field { get; set; }
+    public string? Email { get; set; }
+    public string? PhoneNumber { get; set; }
+    [Required] public string NationalCode { get; set; }
+    public DateOnly? GraduationDate { get; set; }
+    public string? YearOfEntry { get; set; }
+
+    public int InstituteId { get; set; }
+    public Institute Institute { get; set; }
+}
