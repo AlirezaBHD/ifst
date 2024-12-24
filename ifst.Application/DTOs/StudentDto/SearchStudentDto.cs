2867253846fdd770846dee3515becf9f82d407c4
Smp <alirezaqxr@gmail.com>
2024-12-24 08:40:29 +0330

    Creat SearchStudentDto

---
+++ ifst.Application/DTOs/StudentDto/SearchStudentDto.cs
@@ -0,0 +1,11 @@
+﻿namespace ifst.API.ifst.Application.DTOs.StudentDto;
+
+public class SearchStudentDto
+{
+    public string? Name { get; set; }
+    public string? LastName { get; set; }
+    public string? NationalCode { get; set; }
+    public string? StudentId { get; set; }
+    public int Page { get; set; } = 1;
+    public int PageSize { get; set; } = 10;
+}
