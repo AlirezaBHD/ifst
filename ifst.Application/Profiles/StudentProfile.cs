95b73f95ced336711c864b876cfed1da4141fabf
Smp <alirezaqxr@gmail.com>
2024-12-24 08:42:08 +0330

    Add Student -> StudentListDto map profile

---
+++ ifst.Application/Profiles/StudentProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using ifst.API.ifst.Application.DTOs.StudentDto;
+using ifst.API.ifst.Domain.Entities;
+
+namespace ifst.API.ifst.Application.Profiles;
+
+public class StudentProfile : Profile
+{
+    public StudentProfile()
+    {
+        CreateMap<Student, StudentListDto>();
+        CreateMap<Student, StudentDto>();
+    }
+}
