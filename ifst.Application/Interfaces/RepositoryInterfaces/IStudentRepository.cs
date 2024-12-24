ba4cdc916957c80672d59d048338cfb91688074f
Smp <alirezaqxr@gmail.com>
2024-12-24 08:43:25 +0330

    Add SearchStudentsAsync to IStudentRepository Interface

--- ifst.Application/Interfaces/RepositoryInterfaces/IStudentRepository.cs
+++ ifst.Application/Interfaces/RepositoryInterfaces/IStudentRepository.cs
@@ -1,7 +1,10 @@
-﻿using ifst.API.ifst.Domain.Entities;
+﻿using ifst.API.ifst.Application.DTOs;
+using ifst.API.ifst.Application.DTOs.StudentDto;
+using ifst.API.ifst.Domain.Entities;
 
 namespace ifst.API.ifst.Application.Interfaces;
 
 public interface IStudentRepository : IRepository<Student>
 {
+    Task<PaginatedResult<StudentListDto>> SearchStudentsAsync(SearchStudentDto searchOptions);
 }
