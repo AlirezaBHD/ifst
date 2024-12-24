af36cc6e112f241aed2990f17dd2dae713da0b68
Smp <alirezaqxr@gmail.com>
2024-12-24 08:43:49 +0330

    Add GetSearchedStudent to IStudentService interface

---
+++ ifst.Application/Interfaces/ServiceInterfaces/IStudentService.cs
@@ -0,0 +1,11 @@
+﻿using ifst.API.ifst.Application.DTOs;
+using ifst.API.ifst.Application.DTOs.StudentDto;
+
+namespace ifst.API.ifst.Application.Interfaces.ServiceInterfaces;
+
+public interface IStudentService
+{
+    Task<PaginatedResult<StudentListDto>> GetSearchedStudent(SearchStudentDto searchOptions);
+
+    Task<StudentDto> GetStudentById(int id);
+}
